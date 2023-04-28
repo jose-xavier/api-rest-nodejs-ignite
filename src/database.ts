bf4e034750a2045a1188dc04ac5e3f5815e9d558
Jośe Xavier <nettoxavier23@gmail.com>
2023-04-27 23:36:51 -0300

    configure Knex and create the first migration

---
+++ src/database.ts
@@ -0,0 +1,15 @@
+import { default as setupKnex, knex } from 'knex'
+
+export const config: Knex.config = {
+  client: 'sqlite',
+  connection: {
+    filename: './db/app.db',
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+}
+
+export const knex = setupKnex(config)
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
-app.get('/', (request, reply) => {
-  return 'Hello world'
+app.get('/', async (request, reply) => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
 })
 
 app
